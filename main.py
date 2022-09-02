3a4fc1d32aef5217d84fe03704fc8cfdee62f100
ENG19CS0070chandrashekar <76110017+ENG19CS0070chandrashekar@users.noreply.github.com>
2022-09-02 19:01:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def count_batteries_by_usage(cycles):
     "highCount": 0}
     
   # Traversing each element in array 
-  for i in cycles:
-    if i < 400 and i > 0:
-      dic["lowCount"] += 1
-    elif i >= 400 and i <= 919:
-      dic["mediumCount"] += 1
-    elif i >= 920:
-      dic["highCount"] += 1
+  for charge in cycles:
+    if charge > 0 :
+      if charge < 400:
+        dic["lowCount"] += 1
+      elif charge >= 400 and charge <= 919:
+        dic["mediumCount"] += 1
+      else:
+        dic["highCount"] += 1
     else:
       pass
   
@@ -21,6 +22,7 @@ def count_batteries_by_usage(cycles):
 
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
+  # Added new test cases
   counts = count_batteries_by_usage([-100, 100, 300, 400, 500, 600, 900, 919, 920, 1000])
   assert(counts["lowCount"] == 2)
   assert(counts["mediumCount"] == 5)
